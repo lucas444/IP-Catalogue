bc6e3c8a1e5b3a434d663a92452e6a1603cad166
Lucas DIAZ <diazlucasnicodeme@gmail.com>
2023-11-10 23:38:58 +0100

    Ajour Scanf POUR LEs PTN D'ippppppppppppppppppppp

--- PROJET/test.c
+++ PROJET/test.c
@@ -1,25 +1,22 @@
 #include <stdio.h>
 
 
-struct res {
-	int ip[4];
+struct reseaux {
+	int ipv4[4];
 };
-
-struct res t(void){
-
-	struct res init = {{192, 168, 1, 1 }};
-
-	return init;
-}
 
 int main (void){
 
+	struct reseaux ip = {{192, 168, 1, 1 }};
 	
-	struct res s = t();
+	printf("test ip : %d.%d.%d.%d\n", ip.ipv4[0], ip.ipv4[1], ip.ipv4[2], ip.ipv4[3] );
 
 
-	//printf("test %d", &ip);
-	printf("test ip : %d.%d.%d.%d\n", s.ip[0], s.ip[1], s.ip[2], s.ip[3] );
+
+	printf("Saisir une ip : \n");
+	scanf("%d.%d.%d.%d", &ip.ipv4[0], &ip.ipv4[1], &ip.ipv4[2], &ip.ipv4[3]);
+
+	printf("test ip : %d.%d.%d.%d\n", ip.ipv4[0], ip.ipv4[1], ip.ipv4[2], ip.ipv4[3] );
 
 	return 0;
 }
